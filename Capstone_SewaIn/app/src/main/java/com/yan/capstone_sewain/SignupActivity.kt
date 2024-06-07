f7505586008a788c6ab52b6caa28f4c8b12e0a3d
Yan Saputra <yansaputra@Random-2.local>
2024-06-07 18:25:48 +0800

    2

--- Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/SignupActivity.kt
+++ Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.yan.capstone_sewain
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
@@ -11,10 +12,12 @@ import androidx.core.view.WindowInsetsCompat
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var backButton: ImageView
+    private lateinit var registerTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        registerTextView = findViewById(R.id.txt_login)
 
         backButton = findViewById(R.id.back1)
 
@@ -22,5 +25,11 @@ class SignupActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         }
+        registerTextView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
