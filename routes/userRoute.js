c0c46b5729bb6dca58971518b12f0a941fb205fa
bagusarya._ <aryawirianda@gmail.com>
2024-06-08 11:39:58 +0800

    new APi-user

---
+++ routes/userRoute.js
@@ -0,0 +1,13 @@
+// menentukan endpoint API atau rute aplikasi
+
+const { signup, login, logout } = require('../controllers/authController');
+
+const router = require('express').Router();
+
+router.route('/signup').post(signup);
+
+router.route('/login').post(login);
+
+router.route('/logout').post(logout);
+
+module.exports = router;
