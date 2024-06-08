5efb1aabc985eb9bc234b06da328d73fc0b8dbae
HifricAldar <hifricaldar40fmail.com@gmail.com>
2024-06-08 12:59:07 +0800

    Revert "new APi-user"
    This reverts commit c0c46b5729bb6dca58971518b12f0a941fb205fa.

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-// mengatur koneksi ke database berdasarkan konfigurasi di config/config.js
-
-const { Sequelize } = require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config');
-const sequelize = new Sequelize(config[env]);
-
-module.exports = sequelize;
