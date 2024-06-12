f1ee1d7d1d8fddac1341db41e6c02d726eb2466a
yansaputra <yansaputra675@gmail.com>
2024-06-13 05:28:07 +0800

    commit sekian

--- Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/SignupActivity.kt
+++ Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/SignupActivity.kt
@@ -2,7 +2,6 @@ package com.yan.capstone_sewain
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -12,7 +11,6 @@ import com.yan.capstone_sewain.database.Database
 
 class SignupActivity : AppCompatActivity() {
 
-    private lateinit var backButton: ImageView
     private lateinit var registerButton: Button
     private lateinit var registerTextView: TextView
     private lateinit var fullnameEditText: EditText
@@ -40,7 +38,6 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        backButton = findViewById(R.id.back1)
         registerButton = findViewById(R.id.btn_R)
         registerTextView = findViewById(R.id.txt_login)
         fullnameEditText = findViewById(R.id.fullname)
@@ -48,10 +45,6 @@ class SignupActivity : AppCompatActivity() {
         passwordEditText = findViewById(R.id.password)
         confirmPassEditText = findViewById(R.id.confirmpass)
 
-        backButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
         registerTextView.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
