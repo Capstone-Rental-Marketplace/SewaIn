751015a20a2d1b726d5ece06722afdbd0685d761
yansaputra <yansaputra675@gmail.com>
2024-06-14 17:15:02 +0800

    commit sekian2

---
+++ Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/admintoko/VerfiyTokoActivity.kt
@@ -0,0 +1,16 @@
+package com.yan.capstone_sewain.admintoko
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yan.capstone_sewain.R
+
+class VerfiyTokoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_verfiy_toko)
+    }
+}
