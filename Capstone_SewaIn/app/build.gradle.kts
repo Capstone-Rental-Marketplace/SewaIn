ae700e30b4e2fa8d7901158aadf6b10017cedd43
yansaputra <yansaputra675@gmail.com>
2024-06-19 19:29:41 +0800

    commit sekian

--- Capstone_SewaIn/app/build.gradle.kts
+++ Capstone_SewaIn/app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.datastore.core.android)
+    implementation(libs.androidx.datastore.preferences.core.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
