c63df51655efcf27b70527b640008cad782fd237
yansaputra <yansaputra675@gmail.com>
2024-06-21 05:57:56 +0800

    new item

--- Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/MainActivity.kt
+++ Capstone_SewaIn/app/src/main/java/com/yan/capstone_sewain/MainActivity.kt
@@ -56,9 +56,15 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize the sample data
         vehicles = listOf(
-            Vehicle(R.drawable.nmaxlarger, "NMax1", "Rp. 200.000", "Rental Hati", 4.1f,"Rental Hati","Denpasar1"),
-            Vehicle(R.drawable.nmaxlarger, "Nmax2", "Rp. 100.000", "Rental Motor", 4.7f,"Rental Hati","kota baru"),
-            Vehicle(R.drawable.nmaxlarger, "Nmax3", "Rp. 300.000", "Rental Lah", 4.4f,"Rental Hati","Denpasar"),
+            Vehicle(R.drawable.nmaxlarger, "Yamaha Nmax", "Rp. 95.000", "My Bike Rental Bali", 4.0f,"My Bike Rental Bali","Kec. Kediri, Kabupaten Tabanan"),
+            Vehicle(R.drawable.yamahamio, "Yamaha Mio", "Rp. 65.000", "Agas Motorbike Rental", 4.7f,"Agas Motorbike Rental","Kec. Kediri, Kabupaten Tabanan"),
+            Vehicle(R.drawable.hondabeat, "Honda Beat", "Rp. 60.000", "SMB Rent Bike Bali", 4.4f,"SMB Rent Bike Bali","Kec. Kuta Utara, Kabupaten Badung"),
+            Vehicle(R.drawable.yamahagear, "Yamaha Gear", "Rp. 75.000", "Ketut motor rental and taxi", 4.0f,"Ketut motor rental and taxi","Kec. Nusa Penida, Kabupaten Klungkung"),
+            Vehicle(R.drawable.yamahasoul, "Yamaha Soul", "Rp. 65.000", "Rental Motor Bike kadek", 4.7f,"Rental Motor Bike kadek","Kec. Abang, Kabupaten Karangasem"),
+            Vehicle(R.drawable.hondapcx, "Honda PCX", "Rp. 90.000", "Bikago Motorbike Rental", 4.4f,"Bikago Motorbike Rental","Kec. Banjar, Kabupaten Buleleng"),
+            Vehicle(R.drawable.hondabeat, "Honda Beat", "Rp. 60.000", "Bali Nice Rental Scooter", 4.0f,"Bali Nice Rental Scooter","Kec. Ubud, Kabupaten Gianyar"),
+            Vehicle(R.drawable.yamahamio, "Yamaha Mio", "Rp. 65.000", "3 Monkeys Rental Bali", 4.7f,"3 Monkeys Rental Bali","Kec. Kuta Utara, Kabupaten Badung"),
+            Vehicle(R.drawable.yamahafazzio, "Yamaha Fazzio", "Rp. 70.000", "cahya ubud rental motorbike", 4.4f,"cahya ubud rental motorbike","Kec. Ubud, Kabupaten Gianyar"),
         )
 
         vehicleAdapter = VehicleAdapter(vehicles)
